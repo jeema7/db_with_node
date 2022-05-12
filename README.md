a7f527efed867141c765b868788dc75044ff5e3a
jeema7 <89801113+jeema7@users.noreply.github.com>
2022-05-12 01:07:14 -0500

    feat: first select with real database

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # db_with_node
 Conexion a base de datos con javascript
+
+
+## Hot to run this code
+
+
+- `npm i`
+- Clone `.env.example into .env file with the credentials`
--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import knex from "knex";
+import knex from 'knex';
 import dotenv from'dotenv';
 dotenv.config();
 
-knex({
+const connection = knex({  // Esta es la configuracion para hacer la conexion con la base de datos 
     client:'pg',
     connection:{
         host: process.env.DB_HOST,//localhost por defecto 
@@ -12,3 +12,10 @@ knex({
         database: process.env.DB_NAME,
     },
 });
+
+const query = connection('film')
+.where('release_year', 2006);
+
+query.then((rows)=>{
+    console.log(rows);
+})
